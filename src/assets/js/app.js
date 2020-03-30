0b255e0332c204501a0722e0ea9810a29bd87ab3
jelena <poznanovicjelena@gmail.com>
2020-03-30 15:58:43 +0200

    Gallery slider thumbnails current, gallery slider info.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,36 +1,37 @@
 // ON DOCUMENT READY
 $(document).ready(() => {
 
-     $('.gallery-slider__main').slick({
- 	slidesToShow: 1,
- 	slidesToScroll: 1,
- 	arrows: true,
- 	fade: true,
-	infinite: true,
- 	asNavFor: '.gallery-slider__thumbnails'
- });
+	$('.gallery-slider__main').slick({
+		slidesToScroll: 1,
+		slidesToShow: 1,
+		arrows: true,
+		fade: true,
+		infinite: true,
+		asNavFor: '.gallery-slider__thumbnails'
+	});
 
- $('.gallery-slider__thumbnails').slick({
+	$('.gallery-slider__thumbnails').slick({
+		centerMode: true,
+		slidesToShow: 14,
+		slidesToScroll: 1,
+		infinite: true,
+		asNavFor: '.gallery-slider__main',
+		dots: false,
+		arrows: false,
+		focusOnSelect: true,
+		variableWidth: true
+	});
 
- 	slidesToScroll: 1,
-	infinite: true,
- 	asNavFor: '.gallery-slider__main',
-	dots: false,
-	arrows: false,
-	focusOnSelect: true,
-	variableWidth: true
- });
+	// Remove active class from all thumbnail slides
+	//  $('.gallery-slider__thumbnails .slick-slide').removeClass('slick-active');
 
- // Remove active class from all thumbnail slides
- $('.gallery-slider__thumbnails .slick-slide').removeClass('slick-active');
+	//  // Set active class to first thumbnail slides
+	//  $('.gallery-slider__thumbnails .slick-slide').eq(0).addClass('slick-active');
 
- // Set active class to first thumbnail slides
- $('.gallery-slider__thumbnails .slick-slide').eq(0).addClass('slick-active');
-
- // On before slide change match active thumbnail to current slide
- $('.gallery-slider__main').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
- 	var mySlideNumber = nextSlide
- 	$('.gallery-slider__thumbnails .slick-slide').removeClass('slick-active');
- 	$('.gallery-slider__thumbnails .slick-slide').eq(mySlideNumber).addClass('slick-active');
-});
+	// On before slide change match active thumbnail to current slide
+	$('.gallery-slider__main').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+		var mySlideNumber = nextSlide
+		$('.gallery-slider__thumbnails .slick-slide').removeClass('slick-active');
+		$('.gallery-slider__thumbnails .slick-slide').eq(mySlideNumber).addClass('slick-active');
+	});
 });
