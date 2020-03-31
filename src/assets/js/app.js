67cb04d698eb0cee138d3fd542e3c1049d1d14b4
jelena <poznanovicjelena@gmail.com>
2020-03-31 15:53:38 +0200

    Page preloader. Header scroll. Change gallery name to portfolio.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,44 +1,57 @@
 // ON DOCUMENT READY
 $(document).ready(() => {
 
-	$('.gallery-slider__main').slick({
+	$('.portfolio-slider__main').slick({
 		slidesToScroll: 1,
 		slidesToShow: 1,
 		arrows: true,
 		fade: true,
 		infinite: true,
-		asNavFor: '.gallery-slider__thumbnails'
+		asNavFor: '.portfolio-slider__thumbnails'
 	});
 
-	$('.gallery-slider__thumbnails').slick({
+	$('.portfolio-slider__thumbnails').slick({
 		centerMode: true,
 		slidesToShow: 14,
 		slidesToScroll: 1,
 		infinite: true,
-		asNavFor: '.gallery-slider__main',
+		asNavFor: '.portfolio-slider__main',
 		dots: false,
 		arrows: false,
 		focusOnSelect: true,
 		variableWidth: true
 	});
 
-	$('.gallery-slider__main').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+	$('.portfolio-slider__main').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
 		var mySlideNumber = nextSlide
-		$('.gallery-slider__thumbnails .slick-slide').removeClass('slick-active');
-		$('.gallery-slider__thumbnails .slick-slide').eq(mySlideNumber).addClass('slick-active');
+		$('.portfolio-slider__thumbnails .slick-slide').removeClass('slick-active');
+		$('.portfolio-slider__thumbnails .slick-slide').eq(mySlideNumber).addClass('slick-active');
 	});
 
-	$('.gallery-slider__info-box').on('click', function(){
+	$('.portfolio-slider__info-box').on('click', function(){
 
 		$(this).removeClass('is-visible');
 
-		$('.gallery-slider__purchase-price').addClass('is-visible');
+		$('.portfolio-slider__purchase-price').addClass('is-visible');
 	})
 	
-	$('.gallery-slider__purchase-price .close').on('click', function(){
+	$('.portfolio-slider__purchase-price .close').on('click', function(){
 
-		$(this).parents('.gallery-slider__purchase-price').removeClass('is-visible');
+		$(this).parents('.portfolio-slider__purchase-price').removeClass('is-visible');
 
-		$('.gallery-slider__info-box').addClass('is-visible');
+		$('.portfolio-slider__info-box').addClass('is-visible');
 	})
+
+	$('.header-main__bottom a').on('click', function(e) {
+		e.preventDefault();
+		let body = $('html, body');
+		body.stop().animate({ scrollTop: $('.news-cards').offset().top}, 500,'swing');
+		// body.stop().animate({scrollTop:0}, 500, 'swing', function() { 
+		// 	alert("Finished animating");
+		//  });
+	})
+});
+
+$(window).on('load', function() {
+    $('#preloader').fadeOut();
 });
